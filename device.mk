e9edb70b91f3a38742c732301dea8d29f4a9fc89
Dyneteve <dyneteve@pixelexperience.org>
2019-12-25 19:27:39 +0100

    mido: Update Dexpreopt flags
     Add NexusLauncherRelease to PRODUCT_DEXPREOPT_SPEED_APPS
     Remove dex flags, they're already enabled on Q

--- device.mk
+++ device.mk
@@ -136,6 +136,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.ir@1.0-service.xiaomi_mido
 
+# Dex
+PRODUCT_DEXPREOPT_SPEED_APPS += \
+    SystemUI \
+    NexusLauncherRelease
+
 # Display
 PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-impl \
