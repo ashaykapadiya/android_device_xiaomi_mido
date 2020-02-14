040147d638e4a0d4eeb0ead799ad86c23b0456d6
Ali Hasan <ahb7671@gmail.com>
2020-02-14 16:53:08 +0500

    mido: import libqti_vndfwk_detect from LA.UM.8.6.r1-03400-89xx.0
    Signed-off-by: Ali Hasan <ahb7671@gmail.com>

--- device.mk
+++ device.mk
@@ -320,6 +320,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_BOOT_JARS += \
     telephony-ext
 
+# fwk_detect
+PRODUCT_PACKAGES += \
+	libqti_vndfwk_detect
+
 # Seccomp policy
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/seccomp_policy/mediacodec.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy
