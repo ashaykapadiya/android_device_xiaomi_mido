ecb640b04ff89ac2bebc51ef81e0cd1ec5b26fc8
Ashay Kapadiya <kapadiyaashay@gmail.com>
2020-03-27 19:40:12 +0530

    Revert "bringup fix"
    This reverts commit a09cdc3f90f46f708db3fc7b4d14819b316ca948.

--- parts/Android.mk
+++ parts/Android.mk
@@ -25,7 +25,7 @@ LOCAL_STATIC_ANDROID_LIBRARIES := \
 
 LOCAL_RESOURCE_DIR := \
     $(LOCAL_PATH)/res \
-    $(TOP)/packages/apps/NitrogenSettings/res
+    $(TOP)/packages/resources/devicesettings/res
 
 LOCAL_PROGUARD_FLAG_FILES := proguard.flags
 
