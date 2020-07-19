d024951b6ffdc5ba22aa08ec824818c9ed25b71f
ashaykapadiya <kapadiyaashay@gmail.com>
2020-07-19 14:32:21 +0000

    mido : Build OOS Launcher & ScreenRecorder
    Signed-off-by: ashaykapadiya <kapadiyaashay@gmail.com>

--- aosp_mido.mk
+++ aosp_mido.mk
@@ -51,3 +51,11 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
 BUILD_FINGERPRINT := "xiaomi/mido/mido:7.0/NRD90M/V9.6.1.0.NCFMIFD:user/release-keys"
+
+# Build with Oneplus Launcher
+export TARGET_LAUNCHER_CHOICE := oplauncher
+
+# Build OOS ScreenRecorder
+TARGET_OOS_RECORDER := true
+
+
