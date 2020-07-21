3e11b845f8d523dc0af8a3de22f6e713afc19824
AshutoshSundresh <62063875+AshutoshSundresh@users.noreply.github.com>
2020-07-21 22:05:05 +0530

    mido: Properly introduce property_override

--- init/init_mido.cpp
+++ init/init_mido.cpp
@@ -33,10 +33,35 @@
 #include "property_service.h"
 #include "log/log.h"
 
+#define _REALLY_INCLUDE_SYS__SYSTEM_PROPERTIES_H_
+#include <sys/_system_properties.h>
+
+#include <android-base/properties.h>
+#include "property_service.h"
+#include "vendor_init.h"
+
 char const *heapgrowthlimit;
 char const *heapminfree;
 
 using android::init::property_set;
+
+void property_override(char const prop[], char const value[])
+{
+    prop_info *pi;
+
+    pi = (prop_info*) __system_property_find(prop);
+    if (pi)
+        __system_property_update(pi, value, strlen(value));
+    else
+        __system_property_add(prop, strlen(prop), value, strlen(value));
+}
+
+void property_override_dual(char const system_prop[], char const vendor_prop[],
+    char const value[])
+{
+    property_override(system_prop, value);
+    property_override(vendor_prop, value);
+}
 
 void check_device()
 {
